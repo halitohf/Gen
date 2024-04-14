d50a594ee22c2e9eb7d1b66800e999590a209122
Jose de Jesus Zaragoza <halitohf@live.com>
2024-04-14 12:27:00 -0600

    Add Pagina Inicio

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,33 @@ import "./ui/global.css";
 
 export default function Page() {
   return (
-    <h1>Hello🌹🌹🌹</h1>
+<body>
+    <div className="greetings">
+        <span>H</span>
+        <span>o</span>
+        <span>l</span>
+        <span>a</span>
+        <span>,</span>
+        <span></span>
+        <span>B</span>
+        <span>O</span>
+        <span>N</span>
+        <span>I</span>
+        <span>T</span>
+        <span>A</span>
+        
+    </div>
+    <div className="description">
+        <span> 💖😊</span>
+    </div>
+    <div className="button">
+        <button className="botones">
+            <a href="flower.html">¡Presiona !</a>
+        </button>
+    </div>
+    <div className="credits">
+        <p>&copy;</p>
+    </div>
+</body>
   )
 }
